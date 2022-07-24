488d54a51c48c052fa25f40a602e932f1901885f
Yanzz <1171692320@qq.com>
2022-07-24 17:45:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yzz
+ * @create 2022-07-24 17:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
